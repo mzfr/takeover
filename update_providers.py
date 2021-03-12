43b810287cee0ad2999782be92dde8e33cdcc325
mzfr <mehtab.zafar98@gmail.com>
2021-03-12 14:18:55 +0530

    Fix for #8

--- update_providers.py
+++ update_providers.py
@@ -1,3 +1,6 @@
+# This script isn't meant to be used with takeover.
+# I made this script just to help me update the providers.json file.
+
 import requests
 import json
 from bs4 import BeautifulSoup
@@ -5,7 +8,7 @@ from bs4 import BeautifulSoup
 URL = "https://github.com/EdOverflow/can-i-take-over-xyz/blob/master/README.md"
 
 r = requests.get(URL)
-soup = BeautifulSoup(r.content, "html")
+soup = BeautifulSoup(r.content, "html.parser")
 
 table = soup.find("table")
 table_body = soup.find_all("tbody")
@@ -14,7 +17,7 @@ data = []
 for row in rows:
     website = {}
     cols = row.find_all("td")
-    if cols[1].text.strip() == "Vulnerable":
+    if cols and cols[1].text.strip() == "Vulnerable":
         website["name"] = cols[0].text.strip()
         website["cname"] = [" "]
         website["respomse"] = [cols[2].text.strip()]
